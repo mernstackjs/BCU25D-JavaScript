c38b206a22cbf9c4d002c52fe3c7b1fd82fcfe23
Ahmed Abdirizaak <mernstack.js@gmail.com>
2026-02-02 22:54:07 +0100

    Lektion 01

---
+++ lektion01/script.js
@@ -0,0 +1,11 @@
+// Använder oss av console objektet för att skica
+
+console.log("Hej på dig!", "Ahmed");
+console.warn("Detta är en varning!!!");
+console.error("Detta är ett fel!");
+console.log(350);
+console.log("350");
+console.log(false);
+console.log({ make: "Volvo", model: "V60" });
+console.table({ make: "Volvo", model: "V60" });
+console.dir({ make: "Volvo", model: "V60" });
